28f945765f1dcb1904370c09fe889e72acbfc8fe
Thomas Ruggeri <truggeri@gmail.com>
2024-12-09 16:09:39 -0800

    chore: Refactor day 1 to parse string

--- day1/main.go
+++ day1/main.go
@@ -3,13 +3,44 @@ package main
 import (
 	"fmt"
 	"slices"
+	"strconv"
+	"strings"
 )
 
+// This is a bit fragile, but the spec doesn't indicate it will ever change
+const INPUT_SEPARATOR = "   "
+
 func main() {
-	fmt.Println(Solve([]uint{3, 4, 2, 1, 3, 3}, []uint{4, 3, 5, 3, 9, 3}))
+	input := `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`
+	fmt.Println(solve(parseInput(input)))
 }
 
-func Solve(a, b []uint) uint {
+func Solve(input string) uint {
+	return solve(parseInput(input))
+}
+
+func parseInput(input string) ([]uint, []uint) {
+	var a, b []uint
+	for _, line := range strings.Split(input, "\n") {
+		values := strings.Split(line, INPUT_SEPARATOR)
+		if len(values) != 2 {
+			continue
+		}
+
+		x, _ := strconv.Atoi(values[0])
+		a = slices.Concat(a, []uint{uint(x)})
+		y, _ := strconv.Atoi(values[1])
+		b = slices.Concat(b, []uint{uint(y)})
+	}
+	return a, b
+}
+
+func solve(a, b []uint) uint {
 	slices.Sort(a)
 	slices.Sort(b)
 	var sum uint = 0
