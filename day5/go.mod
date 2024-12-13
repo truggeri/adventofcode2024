4c6aa0bbec881c06d622cdab19ee6f2ad7a13a6e
Thomas Ruggeri <truggeri@gmail.com>
2024-12-13 13:54:36 -0800

    feat: Solving day 5

---
+++ day5/go.mod
@@ -0,0 +1,3 @@
+module github.com/truggeri/adventofcode2024/day5
+
+go 1.23.4
---
+++ day5/main_test.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSample(t *testing.T) {
+	input := `47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47`
+	result := Solve(input)
+	if result != 143 {
+		t.Errorf("Calculated solution was not expected")
+	}
+}
