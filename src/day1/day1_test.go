6cb283d4e4edf5441aa3df2c4e1604b6fdf996ce
Thomas Ruggeri <truggeri@gmail.com>
2024-12-28 01:09:08 -0800

    feat: Solving day1 part two

--- src/day1/day1_test.go
+++ src/day1/day1_test.go
@@ -16,3 +16,16 @@ func TestSample(t *testing.T) {
 		t.Errorf("Calculated solution was not expected")
 	}
 }
+
+func TestSamplePartTwo(t *testing.T) {
+	input := `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`
+	result := PartTwo(input)
+	if result != 31 {
+		t.Errorf("Calculated solution was not expected")
+	}
+}
