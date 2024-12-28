35ccb8645f942836869e942f9a9864fae5feb452
Thomas Ruggeri <truggeri@gmail.com>
2024-12-28 01:09:19 -0800

    feat: Solving day2 part two

--- src/day2/day2_test.go
+++ src/day2/day2_test.go
@@ -16,3 +16,64 @@ func TestSample(t *testing.T) {
 		t.Errorf("Calculated solution was not expected")
 	}
 }
+
+func TestSamplePartTwo(t *testing.T) {
+	input := `7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9`
+	result := PartTwo(input)
+	if result != 4 {
+		t.Errorf("Calculated solution was not expected")
+	}
+}
+
+func TestPartTwoFirstTooLarge(t *testing.T) {
+	input := `1 5 6 7 8`
+	result := PartTwo(input)
+	if result != 1 {
+		t.Errorf("Calculated solution was not expected")
+	}
+}
+
+func TestPartTwoFirstThrowsTraj(t *testing.T) {
+	input := `4 5 4 3 2`
+	result := PartTwo(input)
+	if result != 1 {
+		t.Errorf("Calculated solution was not expected")
+	}
+}
+
+func TestPartTwoLastTooLarge(t *testing.T) {
+	input := `1 2 3 4 9`
+	result := PartTwo(input)
+	if result != 1 {
+		t.Errorf("Calculated solution was not expected")
+	}
+}
+
+func TestPartTwoLastThrowsTraj(t *testing.T) {
+	input := `5 4 3 2 3`
+	result := PartTwo(input)
+	if result != 1 {
+		t.Errorf("Calculated solution was not expected")
+	}
+}
+
+func TestPartTwo2ndLastTooLarge(t *testing.T) {
+	input := `9 8 7 4 5`
+	result := PartTwo(input)
+	if result != 1 {
+		t.Errorf("Calculated solution was not expected")
+	}
+}
+
+func TestPartTwo2ndLastThrowsTraj(t *testing.T) {
+	input := `5 4 3 4 1`
+	result := PartTwo(input)
+	if result != 1 {
+		t.Errorf("Calculated solution was not expected")
+	}
+}
